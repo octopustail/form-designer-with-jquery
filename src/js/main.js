f8f1d92b30e36b95e7166467f06d379413f428cd
octopustail <494479914@qq.com>
2018-06-14 10:58:01 +0800

    修改了dialog的问题

--- src/js/main.js
+++ src/js/main.js
@@ -4,67 +4,68 @@
 
 $(function () {
 
-    $(document).ready(function () {
-        $('.addField').on('click', addField);
-        $('#addDate').on('click', addDate);
-        $('#addText').on('click', addText);
-        $('.form').on('click', deletefield);
-        $('.viewField').on('click', viewForm);
 
+    $('.addField').on('click', addField);
+    $('#addDate').on('click', addDate);
+    $('#addText').on('click', addText);
+    $('.form').on('click', deletefield);
+    $('.viewField').on('click', viewForm);
+
+
+    $(".selectField").dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true
     });
 
-    let addField = function () {
-
-        $(".selectField").dialog({
-            resizable: false,
-            height: "auto",
-            width: 400,
-            modal: true
-        });
-
-
-    };
-
-    let addDate = function () {
-        $('.form').append(`<div class="date-input"><input type="date" value="请在这里是输入"><span><button class="delete">delete</button></span></div>`)
-    };
-    let addText = function () {
-        $('.form').append(`<div class="text-input"><input type="text" value="请在这里是输入"><span><button class="delete">delete</button></span></div>`)
-
-    };
-
-    let selectField = function () {
-
-    };
-
-    /**
-     *删除字段
-     * */
-    let deletefield = function (e) {
-        if (e.target.className === 'delete') {
-
-            $(e.target).parents(".text-input").remove();
-            $(e.target).parents(".date-input").remove();
-
-        }
-    };
-
-    /**
-     * 预览表单
-     * */
-    let viewForm = function () {
-        if ($('.switch-mode').text() === '预览') {
-            $('.switch-mode').text('编辑');
-            $('.delete').hide();
-            $('.addField').hide();
-        } else {
-            $('.switch-mode').text('预览');
-            $('.delete').show();
-            $('.addField').show();
-        }
-
-
-    };
 
 });
+
+let addField = function () {
+    $(".selectField").dialog('open');
+};
+
+let addDate = function () {
+    $('.form').append(`<div class="date-input"><input type="date" value="请在这里是输入"><span><button class="delete">delete</button></span></div>`);
+    $(".selectField").dialog('close');
+
+};
+let addText = function () {
+    $('.form').append(`<div class="text-input"><input type="text" value="请在这里是输入"><span><button class="delete">delete</button></span></div>`)
+    $(".selectField").dialog('close');
+
+};
+
+
+/**
+ *删除字段
+ * */
+let deletefield = function (e) {
+    if (e.target.className === 'delete') {
+
+        $(e.target).parents(".text-input").remove();
+        $(e.target).parents(".date-input").remove();
+
+    }
+};
+
+/**
+ * 预览表单
+ * */
+let viewForm = function () {
+    if ($('.switch-mode').text() === '预览') {
+        $('.switch-mode').text('编辑');
+        $('.delete').hide();
+        $('.addField').hide();
+    } else {
+        $('.switch-mode').text('预览');
+        $('.delete').show();
+        $('.addField').show();
+    }
+
+
+};
+
+
 
